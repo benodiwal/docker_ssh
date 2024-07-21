37466d8f1f154361d3950a26ab400a8762127a46
Sachin <s474996633@gmail.com>
2024-07-21 22:11:02 +0530

    run docker fn

--- go.mod
+++ go.mod
@@ -3,9 +3,26 @@ module github.com/benodiwal/docker_ssh
 go 1.22.2
 
 require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/docker v27.0.3+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gliderlabs/ssh v0.3.7 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.53.0 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 )
--- pkg/ssh/docker.go
+++ pkg/ssh/docker.go
@@ -1,10 +1,15 @@
 package ssh
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/benodiwal/docker_ssh/pkg/env"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/gliderlabs/ssh"
 )
 
@@ -24,4 +29,86 @@ func Init() {
 	log.Println("starting ssh server on port 2222 ...")
 	PORT := fmt.Sprintf(":%s", env.Read(env.PORT))
 	log.Fatal(ssh.ListenAndServe(PORT, nil))
+}
+
+type Cleanup func ()
+
+func runDocker(cfg *container.Config, sess ssh.Session) (status int64, cleaup Cleanup, err error) {
+	docker, err := client.NewClientWithOpts()
+	if err != nil {
+		log.Fatalf("Failed to create docker %s", err)
+	}
+
+	status = 255
+	ctx := context.Background()
+
+	res, err := docker.ContainerCreate(ctx, cfg, nil, nil, nil, "")
+	if err != nil {
+		return
+	}
+	cleaup = func() {
+		docker.ContainerRemove(ctx, res.ID, container.RemoveOptions{})
+	}
+
+	opts := container.AttachOptions {
+		Stdin: cfg.AttachStdin,
+		Stdout: cfg.AttachStdout,
+		Stderr: cfg.AttachStderr,
+		Stream: true,
+	}
+	stream, err := docker.ContainerAttach(ctx, res.ID, opts)
+	if err != nil {
+		return
+	}
+	cleaup = func() {
+		docker.ContainerRemove(ctx, res.ID, container.RemoveOptions{})
+		stream.Close()
+	}
+
+	outputErr := make(chan error)
+
+	go func() {
+		var err error
+		if cfg.Tty {
+			_, err = io.Copy(sess, stream.Reader)
+		} else {
+			_, err = stdcopy.StdCopy(sess, sess.Stderr(), stream.Reader)
+		}
+		outputErr <- err
+	}()
+
+	go func() {
+		defer stream.CloseWrite()
+		io.Copy(stream.Conn, sess)
+	}()
+
+	err = docker.ContainerStart(ctx, res.ID, container.StartOptions{})
+	if err != nil {
+		return
+	}
+	if cfg.Tty {
+		_, winCh, _ := sess.Pty()
+		go func() {
+			for win := range winCh {
+				err := docker.ContainerResize(ctx, res.ID, container.ResizeOptions{
+					Height: uint(win.Height),
+					Width: uint(win.Width),
+				})
+				if err != nil {
+					log.Println(err)
+					break
+				}
+			}
+		}()
+	}
+
+	resultC, errC := docker.ContainerWait(ctx, res.ID, container.WaitConditionNotRunning)
+	select {
+	case err = <-errC:
+		return
+	case result := <-resultC:
+		status = result.StatusCode	
+	}
+	err = <-outputErr
+	return
 }
